a3000f1cbe2742ec5c02d70f239e12798ac9ba10
ArtemPER0V <98822741+ArtemPER0V@users.noreply.github.com>
2022-02-05 21:11:09 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -17,3 +17,47 @@ function input_changed () {
         document.getElementById("input-card-number").value = string; /*Add sep when we have 4 elements group*/
     }
 }
+
+function input_fixed () {
+    string = document.getElementById("input-card-number").value;
+    evens = 0;
+    odds = 0;
+    transformed_string = string.split(sep).join('');
+    console.log(transformed_string)
+    for(let i = 1; i < transformed_string.length + 1; i++) {
+        if(i % 2 == 1) {
+            odds += parseInt(transformed_string[i - 1])
+        }
+        else {
+            if(parseInt(transformed_string[i - 1]) * 2 > 9) {
+                evens += parseInt(transformed_string[i - 1]) * 2 - 9;
+            }
+            else {
+                evens += parseInt(transformed_string[i - 1]) * 2;
+            }
+        }
+    }
+
+    if((evens + odds) == 0 && parseInt(transformed_string) != 0) {
+        document.getElementById("wrapper").classList.remove("background-green");
+        document.getElementById("wrapper").classList.remove("background-red");
+        document.getElementById("window__help").innerText = "Введите номер карты";
+        document.getElementById("window__smiley").innerText = ":|";
+        
+    }
+    else if((evens + odds) % 10 == 0) {
+        document.getElementById("wrapper").classList.remove("background-red");
+        document.getElementById("wrapper").classList.add("background-green");
+        
+        document.getElementById("window__help").innerText = "Ваша карта валидная";
+        document.getElementById("window__smiley").innerText = ":)";
+    }
+    else {
+        document.getElementById("wrapper").classList.remove("background-green");
+        document.getElementById("wrapper").classList.add("background-red");
+        document.getElementById("window__help").innerText = "Ваша карта невалидная";
+        document.getElementById("window__smiley").innerText = ":(";
+    }
+
+}
+
